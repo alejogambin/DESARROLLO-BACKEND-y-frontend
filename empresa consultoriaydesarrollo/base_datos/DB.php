6910b19606dd4e8a4e8dc2beeefced7211ce85f6
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-04-17 21:32:46 -0400

    conexion a base datos

--- empresa consultoriaydesarrollo/base_datos/DB.php
+++ empresa consultoriaydesarrollo/base_datos/DB.php
@@ -1,3 +1,8 @@
 <?php   
-echo "<h1>Base de datos</h1>";
+    echo "<h1>Base de datos</h1>";
+    echo "<h2>Conexión a la base de datos</h2>";
+    $conexion = null;
+    $conexion = new PDO('mysql:host=localhost;dbname=consultoraig', 'root', '');
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    ECHO "<h2>Conexión exitosa</h2>";
 ?>
