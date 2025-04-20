7b0f09bb99bb603bc95e1c38f320b6b158661749
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-04-20 15:59:34 -0400

    COMENTARIOS CODIGO

--- empresa consultoriaydesarrollo/js/componente_navbar_toggler.js
+++ empresa consultoriaydesarrollo/js/componente_navbar_toggler.js
@@ -1,5 +1,7 @@
 // Script para el botón de "Scroll to Top"
 document.addEventListener("click", function (event) {
+        ///variable que contiene el elemento del navbar colapsado
+        /// que se utiliza para verificar si el navbar está abierto o cerrado
         const navbarToggler = document.querySelector(".navbar-toggler");
         const navbarCollapse = document.querySelector(".navbar-collapse");
 
